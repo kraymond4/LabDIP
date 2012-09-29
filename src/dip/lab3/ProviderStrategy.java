059452b693ffd1924e3d817fa8792789e38da90f
Owner <Owner@Owner-PC>
2012-09-29 09:52:52 -0500

    Lab3

---
+++ src/dip/lab3/ProviderStrategy.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dip.lab3;
+
+/**
+ *
+ * @author Owner
+ */
+public interface ProviderStrategy 
+{
+    public abstract void setMessage(String message);
+    
+    public abstract String getMessage();
+}
---
+++ src/dip/lab3/RendererStrategy.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dip.lab3;
+
+/**
+ *
+ * @author Kyle Raymond
+ */
+public interface RendererStrategy 
+{
+    public abstract void displayMessage(String message);
+}
