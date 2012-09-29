f8747ae28bc845b2483640ee8c34b5e78b1b1ae1
Owner <Owner@Owner-PC>
2012-09-29 10:10:27 -0500

    Lab3

---
+++ src/dip/lab3/GUIProviderStrategy.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dip.lab3;
+
+/**
+ *
+ * @author Kyle Raymond
+ */
+public class GUIProviderStrategy implements ProviderStrategy
+{
+
+    public void setMessage(String message) 
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public String getMessage() 
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
